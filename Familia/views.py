92592efd7a4004cb1a8234e26af64c5750e88d7e
Vlry777 <camiferulloa@gmail.com>
2023-01-03 21:38:46 -0300

    fin

--- Familia/views.py
+++ Familia/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from Familia.models import Family
 
-def create_family_memebers(request):
-    create_familiar= Family.objects.create(name = 'Sol', surname = 'Hernandez',
-     age = 12, dni= 48820069, alive= True)
+def create_family_memebers(request,name,surname,age,dni,alive):
+    create_familiar= Family.objects.create(name = name, surname = surname,
+     age = age, dni= dni, alive= alive)
     print(create_familiar)
     return HttpResponse('Familiar añadido')
 
